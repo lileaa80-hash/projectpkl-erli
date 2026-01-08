c4565c2ff598c8b2cb25627c74e28b4a689c0bb6
celinn <lileaa80@gmail.com>
2026-01-08 07:47:31 +0700

    ting

--- database/migrations/2025_12_16_072515_create_categories_table.php
+++ database/migrations/2025_12_16_072515_create_categories_table.php
@@ -1,5 +1,4 @@
 <?php
-// database/migrations/xxxx_create_categories_table.php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -10,7 +9,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-
             $table->id();
             $table->string('name', 100);
             $table->string('slug', 100)->unique();
